c536199f9f16addc33c3e236f3e0ee3978c77145
iLama <tjsimpson+github@gmail.com>
2017-03-30 14:07:09 -0700

    Misc. Updates to Chrome and Firefox
    - Removed Firefox extension since the Overlay extension format is being
    deprecated in favor of WebExtension
    - Move Chrome extension to WebExtension.
    - Changed references to MLKSHK to MLTSHP
    - Updated imagery

---
+++ WebExtension/background.js
@@ -0,0 +1,30 @@
+/**                                                                                
+ * Returns a handler which will open a new window when activated.                  
+ */
+function getClickHandler() {
+    return function(info, tab) {
+        source_url = tab.url
+        if (source_url == info.srcUrl) {
+            path_array = source_url.split('/');
+            source_url = path_array[0] + "//" + path_array[2];
+        }
+        chrome.windows.create({
+            "url": "https://mltshp.com/tools/p?url=" + escape(info.srcUrl) + "&source_url=" + escape(source_url),
+            "type": 'popup',
+            "height": 650,
+            "width": 850,
+            "top": (screen.height / 2) - (650 / 2),
+            "left": (screen.width / 2) - (850 / 2)
+        });
+
+    };
+};
+/**                                                                                
+ * Create a context menu which will only show up for images.                       
+ */
+chrome.contextMenus.create({
+    "title": "Save this image on MLTSHP",
+    "type": "normal",
+    "contexts": ["image"],
+    "onclick": getClickHandler()
+});
--- firefox/README.md
+++
@@ -1,3 +0,0 @@
-#MLKSHK Firefox Add-On
-
-Adds a contextual menu item that allows you to right-click on an image and add it to your shake.
