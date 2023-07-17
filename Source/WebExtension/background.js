dba4de3bad1a624824b8417268269c1acba40e93
Dan Phiffer <dan@phiffer.org>
2023-07-17 10:22:27 -0400

    save the alt text along with the file

--- Source/WebExtension/background.js
+++ Source/WebExtension/background.js
@@ -1,5 +1,5 @@
-/**                                                                                
- * Returns a handler which will open a new window when activated.                  
+/**
+ * Returns a handler which will open a new window when activated.
  */
 function getClickHandler() {
     return function(info, tab) {
@@ -8,19 +8,20 @@ function getClickHandler() {
             path_array = source_url.split('/');
             source_url = path_array[0] + "//" + path_array[2];
         }
-        chrome.windows.create({
-            "url": "https://mltshp.com/tools/p?url=" + escape(info.srcUrl) + "&source_url=" + escape(source_url),
-            "type": 'popup',
-            "height": 650,
-            "width": 850,
-            "top": (screen.height / 2) - (650 / 2),
-            "left": (screen.width / 2) - (850 / 2)
+        chrome.tabs.sendMessage(tab.id, "getAltText", { frameId: info.frameId }, data => {
+            chrome.windows.create({
+                "url": "https://mltshp.com/tools/p?url=" + escape(info.srcUrl) + "&source_url=" + escape(source_url) + '&alt=' + escape(data.alt),
+                "type": 'popup',
+                "height": 650,
+                "width": 850,
+                "top": (screen.height / 2) - (650 / 2),
+                "left": (screen.width / 2) - (850 / 2)
+            });
         });
-
     };
 };
-/**                                                                                
- * Create a context menu which will only show up for images.                       
+/**
+ * Create a context menu which will only show up for images.
  */
 chrome.contextMenus.create({
     "title": "Save this image on MLTSHP",
